6d08e7e737ff17a2284941bf6ebba0dc3477c743
btskinner <btskinner@coe.ufl.edu>
2020-01-17 16:43:08 -0500

    Init first function to create a resampling data frame for cross validation. It is mostly just a wrapper for the various tidymodels resampling methods.

---
+++ scripts/r/func_cv.r
@@ -0,0 +1,45 @@
+################################################################################
+##
+## [ PROJ ] Unrollment project
+## [ FILE ] func_cv.r
+## [ AUTH ] Benjamin Skinner (@btskinner)
+## [ INIT ] 17 january 2020
+##
+################################################################################
+
+## -- PURPOSE ------------------------------------------------------------------
+##
+## Function that will restructure a tibble for cross validation:
+##
+## INPUT: tibble of analysis-ready data
+## ARGUMENTS: tbl_df, resampling method, appropriate arguments for method
+## OUTPUT: tibble structured for cross validation
+##
+## NOTES:
+## - using {tidymodels} framework
+##
+## -----------------------------------------------------------------------------
+
+make_cv_df <- function(df,
+                       tm_resample_method,
+                       ...
+                       ) {
+
+    ## requires tidymodels; stop if not installed
+    if (!require("tidymodels")) { stop("tidymodels not installed") }
+
+    if (!is.character(tm_resample_method)) {
+        ## convert if argument name not already a string...
+        fun_name <- deparse(substitute(tm_resample_method))
+    } else {
+        ## ...else leave as is
+        fun_name <- tm_resample_method
+    }
+
+    ## get function from tidymodels
+    fun <- get(fun_name, asNamespace("tidymodels"))
+
+    ## call argument with arguments
+    fun(df, ...)
+
+}
