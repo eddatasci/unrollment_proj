ca8888248b05fdb914ff9c02db7ff64b14766cc8
Will Doyle <wdoyle42@gmail.com>
2020-07-31 16:28:43 -0500

    factors handled, added age

--- scripts/r/data_prep.R
+++ scripts/r/data_prep.R
@@ -1,8 +1,10 @@
 
+
 library(tidyverse)
 library(tidymodels)
 library(workflows)
 library(tune)
+library(lubridate)
 
 
 ## top-level directory; everything else is relative to here
@@ -22,15 +24,24 @@ els_rds <- "els.rds"
 
 
 ## quick paste functions
-`%+%` <- function(a, b) paste(a, b, sep = "")   # a %+% b --> ab
-`%_%` <- function(a, b) paste(a, b, sep = "_")  # a %_% b --> a_b
-`%.%` <- function(a, b) paste(a, b, sep = ".")  # a %.% b --> a.b
+`%+%` <- function(a, b)
+  paste(a, b, sep = "")   # a %+% b --> ab
+`%_%` <- function(a, b)
+  paste(a, b, sep = "_")  # a %_% b --> a_b
+`%.%` <- function(a, b)
+  paste(a, b, sep = ".")  # a %.% b --> a.b
 
 ## are unique values of x < n (default = 20) ? TRUE : FALSE
-few_unique <- function(x, n = 20) { ifelse(length(unique(x)) < n, TRUE, FALSE) }
+few_unique <-
+  function(x, n = 20) {
+    ifelse(length(unique(x)) < n, TRUE, FALSE)
+  }
 
 ## does x %in% val ? NA : x
-recode_missing <- function(x, val) { ifelse(x %in% val, NA, x) }
+recode_missing <- function(x, val) {
+  ifelse(x %in% val, NA, x)
+}
+
 
 ## ------------
 ## macros
@@ -39,26 +50,40 @@ recode_missing <- function(x, val) { ifelse(x %in% val, NA, x) }
 ## unpack the ELS zip file regardless of whether RDS exists? TRUE : FALSE
 unzip_again <- TRUE
 
+## Constants
+
 ## missing values used across ELS variables
 ## CAUTION: these are appropriate for variables we select, but may not
 ## be for all ELS variables; change as appropriate
-els_missing_vals <- c(-1, -3, -4, -7,-8, -9)
+els_missing_vals <- c(-1,-3,-4,-7, -8,-9)
+
+## Proportion to include in a factor: what's the minimum proportion of cases to include
+## as a factor level
+
+factor_prop = .05
+
+## Number of distinct cases to identify a factor: what's the number of different
+## levels that differentiates between factors and not?
+
+n_levels = 20
 
 ## check if working (RDS) file exists
 if (unzip_again || !file.exists(file.path(cddir, els_rds))) {
   ## ...if not, check if working (RDS) file exists
   if (!file.exists(file.path(rddir, els_zip))) {
     ## ...if not, download zip file from NCES
-    download.file(url = els_url,
-                  destfile = file.path(rddir, els_zip),
-                  mode = "wb")
+    download.file(
+      url = els_url,
+      destfile = file.path(rddir, els_zip),
+      mode = "wb"
+    )
   }
   ## ...if so (or after download), unzip and munge
   df <- read_csv(file.path(rddir, els_zip)) %>%
     ## lower variable names
     rename_all(tolower)
   ## save for the future
-#  saveRDS(df, file = file.path(cddir, els_rds))
+  #  saveRDS(df, file = file.path(cddir, els_rds))
 } else {
   ## ...if exists, just read in
   df <- readRDS(file = file.path(cddir, els_rds))
@@ -85,23 +110,27 @@ els <- df %>%
       starts_with("f2b")
       ## Second follow up, ps vars
     )
-  ) %>% ## Second follow up, college vars
-  select_if(negate ( ~ mean(.) == -5)) %>% ##Drop all suppressed vars
-  select_at(vars(-matches("^([bf][y12][q])"))) %>% ## No questionnaire variables
+  ) %>%
+  select_if(negate (~ mean(.) == -5)) %>% ##Drop all suppressed vars
   select_at(vars(-matches("[f[l?]g$"))) %>% ## No flag variables
-  select_at(vars(-matches("wt$")))%>% ## No weight variables
+  select_at(vars(-matches("wt$"))) %>% ## No weight variables
   ## limit to students to started at four-year institutions
-  filter(f2ps1sec %in% c(1:3)) %>% 
+  filter(f2ps1sec %in% c(1:3)) %>%
   ## recode missing values to proper NA
-  mutate_all( ~ recode_missing(.x, els_missing_vals)) %>%
+  mutate_all(~ recode_missing(.x, els_missing_vals)) %>%
   ## create variable for completing a BA/BS
-  mutate(ba_complete = ifelse(f3attainment %in% c(6,7,8,10), 1, 0))%>%
-  ## identify factors and recode as, then lump to no more than 5
-  mutate_if(~(n_distinct(.)<20),as.factor)%>%
+  mutate(ba_complete = ifelse(f3attainment %in% c(6, 7, 8, 10), 1, 0)) %>%
+  ## identify factors and recode as, then lump to no more than specified prop
+  mutate_if( ~ (n_distinct(.) < 20), as.factor) %>%
+  mutate_if(is.factor,  ~ (fct_lump(., p = factor_prop))) %>% # lump rare factors
   ## better factor levels for outcome
   mutate(ba_complete = recode_factor(ba_complete,
                                      "1" = "Completed",
-                                     "0" = "Incomplete"))
-  
-
-        
+                                     "0" = "Incomplete")) %>%
+  ## Age in years (measured only at months)
+  mutate(dob = paste0(bydob_p, "01")) %>% ## add a day for ymd
+  mutate(dob = ymd(dob)) %>% ## convert
+  mutate(date_of_survey = paste0(byqxdatp, "01")) %>% ## add a day for ymd
+  mutate(date_of_survey = ymd(date_of_survey)) %>% ## convert
+  mutate(age_interval = dob %--% date_of_survey) %>% ## get interval
+  mutate(age = (age_interval %/% months(1)) / 12) ## convert to months, months/12=years
